38f52ce49ec745d21a91077b647127873c100b14
Anika Arora <anika_arora@umail.ucsb.edu>
2021-01-23 20:13:08 -0800

    updated test case

--- src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from anika_arora!";
+        return "Greetings from anika-arora!";
     }
     
 }
